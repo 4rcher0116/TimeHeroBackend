e890d11f6a889546edc3fb04f0918364814aae71
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-10-31 12:11:13 -0500

    Correct Databse Path

--- time-hero-backend/config/db.js
+++ time-hero-backend/config/db.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../sqlite/timehero.db');
+console.log(__dirname)
+
+const dbPath = path.resolve(__dirname, '../../sqlite/timehero.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
